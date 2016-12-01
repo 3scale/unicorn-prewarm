b685f3c23b58b24fdcf8419daa5d462d34f3c0d0
Winfield Peterson <winfield.peterson@gmail.com>
2016-12-01 14:41:01 -0500

    prewarm() accepts options hash and custom headers

--- lib/unicorn/prewarm.rb
+++ lib/unicorn/prewarm.rb
@@ -2,8 +2,14 @@ require 'unicorn'
 require 'unicorn/prewarm/version'
 
 module Unicorn
-  def self.prewarm(server)
+  def self.prewarm(server, options={})
     get = Net::HTTP::Get.new('/')
+
+    # Inject custom HTTP Request headers if provided
+    if options[:headers]
+      options[:headers].each {|key, value| get[key.to_s] = value }
+    end
+
     fake = Unicorn::Prewarm::FakeSocket.new(get)
     server.send(:process_client, fake)
     fake.response
